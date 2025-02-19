964d0de08c92a72fb169b55ee9f4562d38bf7073
ComradeChandra <chandrahr@gmail.com>
2025-02-19 16:23:07 +0700

    Commit tygas

--- TugasDanPertemuanCoding/TugasPertemuan3/Test2.java
+++ TugasDanPertemuanCoding/TugasPertemuan3/Test2.java
@@ -1,3 +1,12 @@
 public class Test2 {
-    
+    public static void main(String[] args) {
+        StrukturList list = new StrukturList();
+        list.addHead(5);
+        list.addHead(4);
+        list.addHead(3);
+
+
+        System.out.println("Elemen :");
+        list.display();
+    }
 }
---
+++ TugasDanPertemuanCoding/TugasPertemuan3/Test3.java
@@ -0,0 +1,33 @@
+public class Test3 {
+
+    //Bagian A//
+    public static void main(String[] args) {
+        StrukturList list = new StrukturList();
+        list.addHead(1);
+        list.addHead(2);
+        list.addHead(3);
+
+
+        System.out.println("Elemen :");
+        list.display();
+    
+    
+    //Bagian B//
+        
+    StrukturList list2 = new StrukturList();
+    list2.addHead(5);
+    list2.addHead(4);
+    list2.addHead(3);
+
+
+    System.out.println("Elemen :");
+    list2.display();
+}
+
+}
+
+
+
+
+  
+
